14d4eb81107e2faf6e30694047aa0a67df1853e9
KHaosSuns <KHaos@0x11.email>
2021-09-29 09:52:17 +0800

    2021/09/29 test:test something

--- api/project/queryAll.js
+++ api/project/queryAll.js
@@ -1,12 +1,38 @@
 const NewProject = require('../../model/NewProject')
+const NewItem = require('../../model/NewItem')
+const Penalty = require('../../model/Penalty')
 const Result = require('../../constants/result')
 
 module.exports = async (req, res) => {
-
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
     await NewProject.findAll({
+        attributes: ['blockNumber', 'logIndex', 'projectId', 'name', 'oper', 'createTime'],
+        limit: limit,
+        offset: (page - 1) * limit,
         order: [['createTime', 'DESC']]
     }).then(data => {
-        res.send(Result.SUCCESS(data))
+        const resList = []
+        for (let datum of data) {
+            const projectId = datum.projectId;
+            const items = NewItem.count({
+                where: {
+                    projectId: projectId
+                }
+            })
+            resList.push({
+                'blockNumber': datum.blockNumber,
+                'logIndex': datum.logIndex,
+                'Project': datum.name,
+                'Admin Address': datum.oper,
+                'Staked': 0,
+                'Items': items,
+                'Penalties': 0,
+                'Randoms': 0,
+                'Time': datum.createTime
+            })
+        }
+        res.send(Result.SUCCESS(resList))
     }).catch(err => {
         res.send(Result.ERROR(err))
     });
