6b421f601ce363baecd3772392f18997cf0202ed
宋胜明 <6910078@gmail.com>
2021-11-09 16:08:46 +0800

    add statistics

--- api/project/queryDetails.js
+++ api/project/queryDetails.js
@@ -1,7 +1,10 @@
 const NewProject = require('../../model/NewProject')
 const Penalty = require('../../model/Penalty')
 const AddBound = require('../../model/AddBound')
+const NewItem = require('../../model/NewItem')
+const NewRandom = require('../../model/NewRandom')
 const Result = require('../../constants/result')
+const db = require('../../config/mysql')
 
 module.exports = async (req, res) => {
 
@@ -11,13 +14,39 @@ module.exports = async (req, res) => {
 
         if (data === null) return res.send(Result.SUCCESS(data));
 
-        const totalStaked = await AddBound.sum('payAmount', {where: {projectId: projectId}}) + data.depositAmt;
-        const totalPenalty = await Penalty.sum('rewardAmount', {where: {projectId: projectId}});
+        const totalStaked = await AddBound.sum('payAmount', { where: { projectId: projectId } }) + data.depositAmt;
+        const totalPenalty = await Penalty.sum('rewardAmount', { where: { projectId: projectId } });
         const totalBalance = totalStaked - totalPenalty;
         const tolerance = Math.min(totalBalance * 10 / data.depositAmt, 10);
         let unstaking = data.status === 1 ? totalBalance : 0;
         let unstaked = data.status === 2 ? totalBalance : 0;
 
+        const penalties = await Penalty.count({ where: { projectId } })
+        const randoms = await NewItem.count({ where: { projectId } })
+        const items = await NewRandom.count({ where: { projectId } })
+
+        // statistics
+        let sql = 'select a.date,IFNULL(b.count,0) count  from ( SELECT CURDATE() AS date '
+        for (let i = 1; i <= 29; i++) {
+            sql += " union all SELECT DATE_SUB(CURDATE(), INTERVAL " + i + " DAY) AS date"
+        }
+        sql += ') a left join (select FROM_UNIXTIME(createTime / 1000,"%Y-%m-%d") date,count(1) count from newrandom group by FROM_UNIXTIME(createTime / 1000,"%Y-%m-%d")) b on a.date = b.date where 1=1'
+
+        sql += ' and projectId = ? '
+
+        sql += ' order by date'
+        const object = await db.query(sql, {
+            replacements: [projectId],
+            type: QueryTypes.SELECT
+        })
+
+        let statistics = []
+
+        if (object) {
+            for (let obj of object[0]) {
+                statistics.push(obj.count)
+            }
+        }
         res.send(Result.SUCCESS({
             'projectID': data.projectId,
             'date': data.createTime,
@@ -27,7 +56,8 @@ module.exports = async (req, res) => {
             'totalFine': totalPenalty,
             'unstaking': unstaking,
             'unstaked': unstaked,
-            'toleranceTimes': tolerance
+            'toleranceTimes': tolerance,
+            penalties, randoms, items, statistics
         }))
     }).catch(err => {
         res.send(Result.ERROR(err))
