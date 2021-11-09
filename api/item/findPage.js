974962cb0d6e0c5a0258ea77ba6d6ad2aaa9489c
宋胜明 <6910078@gmail.com>
2021-11-09 14:02:22 +0800

    fix

--- api/item/findPage.js
+++ api/item/findPage.js
@@ -5,18 +5,24 @@ const { QueryTypes } = require('sequelize')
 module.exports = async (req, res) => {
     const limit = req.query.limit ? parseInt(req.query.limit) : 10
     const page = req.query.page ? parseInt(req.query.page) : 1
+    const projectId = req.query.projectId
 
     let sql = 'select '
-        + ' a.blockNumber,a.logIndex,a.projectId,a.name project,a.oper adminAddress,a.depositAmt staked,a.createTime time, '
-        + ' (select count(1) from newitem where projectId = a.projectId) items,'
-        + ' (select count(1) from Penalty where projectId = a.projectId) penalties,'
-        + ' (select count(1) from NewRandom where projectId = a.projectId) randoms'
-        + ' from newproject a '
+        + ' a.blockNumber, a.logIndex, a.blockHash hash, a.projectId, b.name project, b.oper adminAddress, a.itemId, a.createTime, '
+        + ' ( select count(1) from penalty where itemid = a.itemid ) penalties,'
+        + ' ( select count(1) from newrandom where itemid = a.itemid ) randoms'
+        + ' from newItem a left join newproject b on a.projectId = b.projectId where 1=1'
+
+    const params = []
+    if (!isNaN(projectId)) {
+        sql += ' and projectId = ?'
+        params.push(projectId)
+    }
 
     const count_sql = 'select count(1) from ( ' + sql + ' ) a '
     sql += ' order by a.createTime desc limit ?,? '
 
-    const replacements = []
+    const replacements = [].concat(params)
     replacements.push((page - 1) * limit)
     replacements.push(limit)
     let list = await db.query(sql, {
@@ -24,7 +30,7 @@ module.exports = async (req, res) => {
         type: QueryTypes.SELECT
     })
     let count = await db.query(count_sql, {
-        replacements: [],
+        replacements: params,
         type: QueryTypes.SELECT
     })
     let data = {
