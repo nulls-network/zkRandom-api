a1765d4ada9f0ec23f2eb6b95261426f2eb35956
宋胜明 <6910078@gmail.com>
2021-11-09 11:39:13 +0800

    fix

--- api/project/findPage.js
+++ api/project/findPage.js
@@ -1,44 +1,38 @@
-// const db = require('../../config/mysql')
-// const Result = require('../../constants/result')
-// const { QueryTypes } = require('sequelize')
+const db = require('../../config/mysql')
+const Result = require('../../constants/result')
+const Penalty = require('../../model/Penalty')
+const NewItem = require('../../model/NewItem')
+const NewRandom = require('../../model/NewRandom')
+const { QueryTypes } = require('sequelize')
 
-// module.exports = async (req, res) => {
-//     const limit = req.query.limit ? parseInt(req.query.limit) : 10
-//     const page = req.query.page ? parseInt(req.query.page) : 1
+module.exports = async (req, res) => {
+    const limit = req.query.limit ? parseInt(req.query.limit) : 10
+    const page = req.query.page ? parseInt(req.query.page) : 1
 
-//     const projectId = req.query.projectId
-//     const itemId = req.query.itemId
+    let sql = 'select '
+        + ' a.blockNumber,a.logIndex,a.projectId,a.name project,a.oper adminAddress,b.depositAmt staked,a.createTime time, '
+        + ' (select count(1) from newitem where projectId = a.projectId) items,'
+        + ' (select count(1) from Penalty where projectId = a.projectId) items,'
+        + ' (select count(1) from NewRandom where projectId = a.projectId) items'
+        + ' from newproject a '
 
-//     let sql = 'select '
-//         +' a.blockNumber,a.logIndex,a.projectId,a.name,a.oper,b.depositAmt,a.createTime '
-//         +' from newrandom a left join newproject b on a.projectId = b.projectId where 1=1 '
+    const count_sql = 'select count(1) from ( ' + sql + ' ) a '
+    sql += ' order by a.createTime desc limit ?,? '
 
-//     const params = []
-//     if (!isNaN(projectId)) {
-//         sql += ' and a.projectId = ? '
-//         params.push(projectId)
-//     }
-//     if (!isNaN(itemId)) {
-//         sql += ' and a.itemId = ? '
-//         params.push(itemId)
-//     }
-//     const count_sql = 'select count(1) from ( ' + sql + ' ) a '
-//     sql += ' order by a.createTime DESC limit ?,? '
-
-//     const replacements = [].concat(params)
-//     replacements.push((page - 1) * limit)
-//     replacements.push(limit)
-//     let list = await db.query(sql, {
-//         replacements,
-//         type: QueryTypes.SELECT
-//     })
-//     let count = await db.query(count_sql, {
-//         replacements: params,
-//         type: QueryTypes.SELECT
-//     })
-//     let data = {
-//         count: count[0]['count(1)'],
-//         row: list
-//     }
-//     res.status(200).json(Result.SUCCESS(data))
-// }
+    const replacements = []
+    replacements.push((page - 1) * limit)
+    replacements.push(limit)
+    let list = await db.query(sql, {
+        replacements,
+        type: QueryTypes.SELECT
+    })
+    let count = await db.query(count_sql, {
+        replacements: [],
+        type: QueryTypes.SELECT
+    })
+    let data = {
+        count: count[0]['count(1)'],
+        row: list
+    }
+    res.status(200).json(Result.SUCCESS(data))
+}
